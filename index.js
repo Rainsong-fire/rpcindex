9e4d41a5d14a3bcc8207c1f3e3f605701052a7ce
rain song <e1900594321@gmail.com>
2021-08-09 16:24:22 +0800

    1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let request = require('request')
 // Call the Infura API and check that the address is valid.
 //this object is used to detail the request to infura
 let options = {
-    url:'http://127.0.0.1:1234/rpc/v1/filecoin',
+    url:'http://127.0.0.1:1234',
     method: 'post',
     headers: { 'content-type' : 'application/json' },
     headers: {'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBbGxvdyI6WyJyZWFkIiwid3JpdGUiLCJzaWduIiwiYWRtaW4iXX0.xJTU4X44u-9GZB4C-t4U6Jt91NlTJqOurnimGN1AjFs'},
