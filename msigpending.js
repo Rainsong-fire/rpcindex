eb27e729ca8e9d7a782a58706be3ff51e1392570
rain song <e1900594321@gmail.com>
2021-08-19 16:13:33 +0800

    1

--- msigpending.js
+++ msigpending.js
@@ -10,7 +10,7 @@ let options = {
     //the last member is the body, where stores the details of api request, such as the method called the params.etc.
     //cautions: the body is a string as a whole!!!!
 
-    body:'{"jsonrpc":"2.0","id":0, "method":"Filecoin.MsigGetPending", "params": ["t012289",[{"/":"bafy2bzacebnopvohdq7z2ipuxw22ahibeympwuobrrleafqauajg2a7p6mndo"},{"/": "bafy2bzacebuuly6o4dfx6orjh5vwgbupt2y3o5tn37bz5umowongfmpr4edgc"}]]}'
+    body:'{"jsonrpc":"2.0","id":0, "method":"Filecoin.MsigGetPending", "params": ["t012289",[{"/":"bafy2bzacedbcd5mg4d7e3bd4l3spkaojbjetg5qq4hbr2x2m7n3j7e5s5z3cu"},{"/": "bafy2bzacebnkpdqe63knjukavddi4yomshtles23sphrsdmyxvsmkvlrenbgq"}]]}'
     //cautions: the params is not string ,it is a list!!!!!!!there is no map in the list, only the params listed by order.
     //params 中，两个tipsetkey 都是放在[]中的。每个都放在{} 内的。
 }
